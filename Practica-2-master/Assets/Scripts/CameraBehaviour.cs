de7cfea5a7a3c4d878422d3833692ca72e292294
ibaibaglietto <ibaibaglietto@gmail.com>
2019-02-21 12:06:06 +0100

    Empezar a gestionar la camara

---
+++ Practica-2-master/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    /* Variables publicas
+     * - Variable objetivo (player)
+     * - ALtura mínima (4f)
+     * - Factor de altura
+     */
+
+    /* Variables privadas
+     * - Altura
+    */
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Mi posicion = posicion de objetivo + Altura
+        // Altura = [Altura mínima, Altura mínima + velocidad objetivo * factor de altura]
+
+    }
+}
