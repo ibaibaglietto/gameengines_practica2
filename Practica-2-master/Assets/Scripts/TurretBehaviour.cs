880f5e8d533404e70be5872b3c7453c901ac21bf
ibaibaglietto <ibaibaglietto@gmail.com>
2019-02-22 13:00:06 +0100

    Empezar con el turret

---
+++ Practica-2-master/Assets/Scripts/TurretBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretBehaviour : MonoBehaviour
+{
+    public Transform cannon;
+    public Transform player;
+    public GameObject bullet;
+    public float shootRange;
+    public float shootFrequency;
+
+    float lastShot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Mathf.Sqrt(Mathf.Pow(transform.position.x - player.transform.position.x, 2) + Mathf.Pow(transform.position.z - player.transform.position.z, 2));
+        print(dist);
+        if (dist < shootRange)
+        {
+            cannon.LookAt(player);
+            shot();
+
+        }
+    }
+
+    void shot()
+    {
+
+    }
+}
